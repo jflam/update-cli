53749149018a412c2257f872bc6ed5722bf64507
John Lam <jlam@iunknown.com>
2024-08-21 18:07:00 -0700

    fix: jest runs tests only once and ignores dist dir

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,13 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+    preset: "ts-jest",
+    testEnvironment: "node",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    testMatch: [
+        "**/__tests__/**/*.[jt]s?(x)",
+        "**/?(*.)+(spec|test).[tj]s?(x)",
+    ],
     transform: {
-      '^.+\\.tsx?$': 'ts-jest',
+        "^.+\\.tsx?$": "ts-jest",
     },
-  };
+    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+};
